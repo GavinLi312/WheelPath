a0a7d620dd25c262a549036d59bb988451f4e831
Gavin Li <salamenderli@dyn-118-138-82-142.its.monash.edu.au>
2018-08-20 14:24:29 +1000

    Version1

---
+++ ProjectVersion1/ProjectVersion1Tests/ProjectVersion1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectVersion1Tests.swift
+//  ProjectVersion1Tests
+//
+//  Created by Salamender Li on 20/8/18.
+//  Copyright © 2018 Salamender Li. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectVersion1
+
+class ProjectVersion1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
